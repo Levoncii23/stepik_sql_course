d7ec9dd4d0ccb9b58a327f53485219c0790462fa
levoncii23 <levoncii23@gmail.com>
2021-11-03 11:37:17 +0300

    1.2.12 task

--- tasks.sql
+++ tasks.sql
@@ -46,3 +46,33 @@ FROM book;
 SELECT author, title,
     ROUND(IF(author = "Булгаков М.А.", price*1.1, IF(author = "Есенин С.А.", price*1.05, price)), 2) AS new_price
 FROM book;
+
+-- 1.2.8 Выборка данных по условию WHERE
+SELECT author, title, price
+FROM book
+WHERE amount < 10;
+
+-- 1.2.9 Лог операции AND OR NOT
+SELECT title, author, price, amount 
+FROM book
+WHERE (price > 500 or price < 600) and price * amount > 5000;
+
+-- 1.2.10 ОПераторы BETWEEN IN
+SELECT title, author 
+FROM book
+WHERE (price BETWEEN 540.50 AND 800) and amount IN (2,3,5,7);
+
+-- 1.2.11 Выборка данных с сортировкой
+SELECT author, title
+FROM book
+WHERE (amount BETWEEN 2 AND 14)
+ORDER BY author DESC, title ASC;
+
+-- 1.2.12 Оператор LIKE (% любася строка ноль или более символов)
+--  (_ любой одиночный символ)
+SELECT title, author
+FROM book
+WHERE (title LIKE "% %" AND title NOT LIKE "_")
+        AND
+        author LIKE "% %%С.%"
+ORDER BY title ASC
